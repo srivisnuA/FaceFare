5aac54ad6aee63e1f89754b53fa4b3bac1417bfa
srivisnu <67585311+srivisnuA@users.noreply.github.com>
2026-09-20 06:10:15 +0530

    use deployment port from environment

--- app.py
+++ app.py
@@ -632,7 +632,7 @@ if __name__ == "__main__":
     socketio.run(
         app,
         host         = "0.0.0.0",
-        port         = 5000,
+        port         = int(os.environ.get("PORT", 5000)),
         debug        = False,
         use_reloader = False,
     )
